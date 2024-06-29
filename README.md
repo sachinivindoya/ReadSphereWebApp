9dd4b8e501a11afb6bb5dbadd0a8f7db9e937afc
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-06-29 11:11:25 +0530

    initializing the project

---
+++ README.md
@@ -0,0 +1,1 @@
+#ReadSphere Book App
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <header>ReadSphere Book Store</header>
+        <main></main>
+        <footer>All Right Reserved</footer>
+    </div>
+    </>
+  )
+}
+
+export default App
